4f6e9a65a4422bae2f7e9fc47ee8e11d437f5962
jaimerzp <jaimerz011235813@gmal.com>
2024-07-23 11:33:01 +0100

    not needed imports

--- src/rail/rail_prior/prior_shifts.py
+++ src/rail/rail_prior/prior_shifts.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy.optimize import fsolve
 from .prior_base import PriorBase
 
 
--- src/rail/rail_prior/prior_shifts_widths.py
+++ src/rail/rail_prior/prior_shifts_widths.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy.stats import multivariate_normal as mvn
 from .prior_base import PriorBase
 
 
