611ef6876102ddd870b12bf23c18c4d1cb791ba5
jaimerzp <jaimerz011235813@gmal.com>
2024-07-26 11:27:33 +0100

    allow for different forms of computing the cross corrs

--- src/rail/rail_prior/prior_sacc.py
+++ src/rail/rail_prior/prior_sacc.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.linalg import cholesky
+from scipy.linalg import block_diag
 from .prior_base import PriorBase
 from .prior_shifts import PriorShifts
 from .prior_shifts_widths import PriorShiftsWidths
@@ -9,7 +10,9 @@ from .utils import make_cov_posdef, is_pos_def
 
 class PriorSacc(PriorBase):
 
-    def __init__(self, sacc_file, model="Shifts"):
+    def __init__(self, sacc_file,
+                 model="Shifts",
+                 compute_crosscorrs="Full"):
         if model == "Shifts":
             self.model = PriorShifts
         if model == "ShiftsWidths":
@@ -17,6 +20,7 @@ class PriorSacc(PriorBase):
         if model == "Moments":
             self.model = PriorMoments
 
+        self.compute_crosscorrs = compute_crosscorrs
         self.tracers = sacc_file.tracers
         self.params = self._find_params()
         self.params_names = self._get_params_names()
@@ -29,17 +33,35 @@ class PriorSacc(PriorBase):
             ens = tracer.ensemble
             model_obj = self.model(ens)
             params_sets = model_obj._get_params()
-            for params_set in params_sets:
-                params.append(params_set)
-
+            params.append(params_sets)
         return np.array(params)
 
     def _get_prior(self):
-        params = self.params
-        mean = np.mean(params, axis=1)
-        cov = np.cov(params)
-        cov = make_cov_posdef(cov)
-        chol = cholesky(cov)
+        mean = np.array([np.mean(param_sets, axis=1) for param_sets in self.params]).flatten()
+        if self.compute_crosscorrs == "Full":
+            params = []
+            for param_sets in self.params:
+                for param_set in param_sets:
+                    params.append(param_set)
+            params = np.array(params)
+            cov = np.cov(params)
+            cov = make_cov_posdef(cov)
+            chol = cholesky(cov)
+        if self.compute_crosscorrs == "BinWise":
+            covs = []
+            for p in self.params:
+                covs.append(np.cov(p))
+            covs = np.array(covs)
+            cov = block_diag(*covs)
+            cov = make_cov_posdef(cov)
+            chol = cholesky(cov)
+        if self.compute_crosscorrs == "None":
+            stds = []
+            for param_sets in self.params:
+                for param_set in param_sets:
+                    stds.append(np.std(param_set))
+            cov = np.diag(stds**2)
+            chol = np.eye(len(mean))
         return mean, cov, chol
 
     def _get_params_names(self):
