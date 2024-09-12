0447ee5b45233da2fa8fa5a3e056a04c3a3f77bd
jaimerzp <jaimerz011235813@gmal.com>
2024-09-12 15:43:18 +0200

    Dkl

--- nz_prior/__init__.py
+++ nz_prior/__init__.py
@@ -5,4 +5,4 @@ from .prior_moments import PriorMoments
 from .prior_comb import PriorComb
 from .prior_sacc import PriorSacc
 from .models import shift_model, shift_and_width_model, comb_model
-from .utils import is_pos_def, make_cov_posdef
+from .utils import is_pos_def, make_cov_posdef, Dkl, Sym_Dkl
